541eddc87a34724987cb3a6f6805c29b49b2fa05
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-07-23 06:13:08 +0530

    initial commit: imported frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MapDrawer from './components/MapDrawer'
+
+export default function App() {
+    return (
+        <main className="h-screen w-full flex flex-col">
+            <MapDrawer />
+            {/* <footer className="mx-auto w-fit text-center bg-transparent">
+          Least Exposure to Air Pollution Path
+        </footer> */}
+        </main>
+    )
+}
